4a2d584fa7a64bbeabcad7c2102e2afbdfcca3f3
Minurian <legacyofspirits167@gmail.com>
2025-03-16 16:37:34 -0400

    Cambios v1
    Se hicieron cambios en el codigo de space shoots y se eliminaron y cambiaron algunos assets

--- PyGame/GameExample/SpaceShooter.py
+++ PyGame/GameExample/SpaceShooter.py
@@ -12,7 +12,7 @@ pygame.init()
 FPS = 60
 FramePerSec = pygame.time.Clock()
 
-# Predefined some colors
+# Predefined some colors    
 BLUE  = (0, 0, 255)
 RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -26,7 +26,7 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 screen.fill(BLACK)
 pygame.display.set_caption("Game") #Titulo
 
-class Enemy(pygame.sprite.Sprite):
+class Enemy1(pygame.sprite.Sprite):
     def __init__(self): #constructor de la clase
         super().__init__() # Llamado al constructor de la clase base
         self.image = pygame.image.load("F:\\Programacion\\Practicas\\PyGame\\GameExample\\Assets\\Images\\Enemy.png") # Carga la imagen del enemigo
@@ -34,10 +34,26 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.center = (random.randint(40, screen_width-40), 0) # Crea una funcion para que aparezcan los enemigos aleatoriamente 
         
     def move(self): # Define como se comportara el movimiento del enemigo
-        self.rect.move_ip(0,5) # Define que el enemigo se movera 10 pixeles hacia abajo
+        self.rect.move_ip(0,2) # Define que el enemigo se movera 10 pixeles hacia abajo
         if(self.rect.bottom > 600): #Condicional que indica si el borde del enemigo pasa la coordenada 600 se volvera a reposicionar dicho enemigo
             self.rect.top = 0
             self.rect.center = (random.randint(30 , 370), 0) #Aca definira donde se ubicara nuevamente
+    
+    def draw (self, surface): # Dibuja al enemigo
+        surface.blit(self.image, self.rect)
+        
+# class Enemy2(pygame.sprite.Sprite):
+#     def __init__(self): #constructor de la clase
+#         super().__init__() # Llamado al constructor de la clase base
+#         self.image = pygame.image.load("F:\\Programacion\\Practicas\\PyGame\\GameExample\\Assets\\Images\\Enemy.png") # Carga la imagen del enemigo
+#         self.rect = self.image.get_rect() # Crea un rectangulo en la imagen que servira para limitar el espacion que abarca y poder crear colisiones
+#         self.rect.center = (random.randint(40, screen_width-40), 0) # Crea una funcion para que aparezcan los enemigos aleatoriamente 
+        
+#     def move(self): # Define como se comportara el movimiento del enemigo
+#         self.rect.move_ip(0,5) # Define que el enemigo se movera 10 pixeles hacia abajo
+#         if(self.rect.bottom > 600): #Condicional que indica si el borde del enemigo pasa la coordenada 600 se volvera a reposicionar dicho enemigo
+#             self.rect.top = 0
+#             self.rect.center = (random.randint(30 , 370), 0) #Aca definira donde se ubicara nuevamente
     
     def draw (self, surface): # Dibuja al enemigo
         surface.blit(self.image, self.rect)
@@ -46,13 +62,14 @@ class Player (pygame.sprite.Sprite):
     def __init__(self, *groups):
         super().__init__(*groups)
         self.image = pygame.image.load("F:\\Programacion\\Practicas\\PyGame\\GameExample\\Assets\\Images\\nave.png")
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect() # Crea un rectangulo en la imagen que servira para limitar el espacion que abarca y poder crear colisiones
                 # obtener ancho y alto de pantalla
         screen_width = pygame.display.get_surface().get_size()[0]
         screen_height = pygame.display.get_surface().get_size()[1]
         self.player_group = pygame.sprite.Group()  # Crea un grupo solo para el jugador
         
         self.rect.center = (screen_width // 2 , screen_height // 2) # ubicacion del sprite. En este caso se hizo un calculo para que se posicionara al centro
+        self.bullets = pygame.sprite.Group() # Grupo para las balas
         
 
     def update(self):
@@ -70,32 +87,42 @@ class Player (pygame.sprite.Sprite):
                 self.rect.move_ip(5, 0)
 
     def disparar(self):
-        bala = bullet(self.rect.centerx, self.rect.top)
-        self.player_group.add(bala)
+        bala = Bullet(self.rect.centerx, self.rect.top)
+        self.bullets.add(bala)
 
-    def draw(self, surface):  # Dibuja al jugador
+    def draw(self, surface):
         surface.blit(self.image, self.rect)
+        self.bullets.draw(surface) 
         
 
-class bullet (pygame.sprite.Sprite): #Creamos una clase para el disparo (bala)
+class Bullet (pygame.sprite.Sprite): #Creamos una clase para el disparo (bala)
     def __init__(self, x , y):
         super().__init__()
         self.image = pygame.image.load("F:\\Programacion\\Practicas\\PyGame\GameExample\\Assets\\Images\\bullett.png")
         self.rect = self.image.get_rect()
         self.rect.centerx = x
         self.rect.bottom = y
-        self.speed_y = -2
+        self.speed_y = -10
     
     def update(self):
         self.rect.y += self.speed_y
-        
         #Eliminamos la bala si sale de la pantalla
         if self.rect.bottom < 0:
             self.kill()
         
 
 P1 = Player()
-E1 = Enemy()
+enemies = pygame.sprite.Group()
+E1 = Enemy1()
+enemies.add(E1)
+
+def create_enemy():
+    enemy = Enemy1()
+    enemies.add(enemy)
+
+create_enemy()
+
+#Bucle principal
 
 while True: 
     for event in pygame.event.get ():
@@ -108,11 +135,21 @@ while True:
             sys.exit()
     
     P1.update()
-    E1.move()
+    for enemy in enemies:
+        enemy.move()
+    P1.bullets.update()
     
-    screen.fill(WHITE)
+    for bullet in P1.bullets:
+        collided_enemies = pygame.sprite.spritecollide(bullet, enemies, True)
+        if collided_enemies:
+            bullet.kill()
+            
+    if random.randint(1, 10) == 1:
+        create_enemy()
+    
+    screen.fill(BLACK)
     P1.draw(screen)
-    E1.draw(screen)
+    enemies.draw(screen)
     
     pygame.display.update()
     FramePerSec.tick(FPS)
